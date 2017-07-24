93577f50b2928349262cb9c47d0112fbc0a067ab
Marvin Kopf <marvinkopf@posteo.de>
2017-07-25 01:06:46 +0200

    parse login modal input

--- ClientApp/app/components/loginModal/loginModal.component.ts
+++ ClientApp/app/components/loginModal/loginModal.component.ts
@@ -41,6 +41,15 @@ export class LoginModalComponent implements ModalComponent<DialogPreset> {
     }
 
     login(): void {
+        this.registerErrorMessage = null;
+        this.loginErrorMessage = null;
+
+        if (this.login_username == null || this.login_password == null) {
+
+            this.loginErrorMessage = "Fields missing.";
+            return;
+        }
+
         this.authenticationService.Login(this.login_username, this.login_password).subscribe(
             null,
             err => this.loginErrorMessage = err,
@@ -49,6 +58,19 @@ export class LoginModalComponent implements ModalComponent<DialogPreset> {
     }
 
     register(): void {
+        this.registerErrorMessage = null;
+        this.loginErrorMessage = null;
+
+        if (this.register_username == null || this.register_password == null ||
+            this.register_email == null) {
+            this.registerErrorMessage = "Fields missing.";
+            return;
+        }
+
+        if (this.register_password != this.register_verifyPassword) {
+            this.registerErrorMessage = "Passwords do not match."
+        }
+
         this.authenticationService.Register(
             this.register_username,
             this.register_password,
